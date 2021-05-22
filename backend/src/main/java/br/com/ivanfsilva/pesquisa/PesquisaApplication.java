889e33cb6f21056258b7e44696bbf205778b5fa6
ivanfsilva <ivanfs27@gmail.com>
2021-05-22 16:05:46 -0300

    Created project

---
+++ backend/src/main/java/br/com/ivanfsilva/pesquisa/PesquisaApplication.java
@@ -0,0 +1,13 @@
+package br.com.ivanfsilva.pesquisa;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PesquisaApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(PesquisaApplication.class, args);
+    }
+
+}
